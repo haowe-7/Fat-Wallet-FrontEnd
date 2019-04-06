9bd2499e9d4e5affc6bd61a77c8fc5966478944f
Carey <944131226@qq.com>
2019-04-07 03:00:04 +0800

    找回密码弹框二界面初步

--- src/components/PasswordBackDialog.vue
+++ src/components/PasswordBackDialog.vue
@@ -1,20 +1,21 @@
 <template>
   <div id="findpass-dialog">
-    <el-dialog :close-on-click-modal="false" :close-on-press-escape="false" custom-class="custom-style" :visible.sync="visible" :before-close="hideDialog">
+    <el-dialog :close-on-click-modal="false" :close-on-press-escape="false" custom-class="custom-style" 
+                :visible.sync="visible" :before-close="hideDialog">
       <div slot="title" class="dialog-title">
-        <br><span id="dialog-text1">找回密码</span><br>
-        <br><span id="dialog-text2">验证码将会发送至你的注册邮箱或手机</span><br>
+        <br><span id="dialog-text1">{{this.text_info.text1}}</span><br>
+        <br><span id="dialog-text2">{{this.text_info.text2}}</span><br>
       </div>
       <div class="dialog-body">
         <el-form> 
           <el-form-item>
-            <el-input placeholder="邮箱/手机号" autocomplete="off"></el-input>
+            <el-input id="input-box1" v-model="contactInfo" placeholder="邮箱/手机号" autocomplete="off" :disabled="input_box1_disabled"></el-input>
           </el-form-item>
           <el-form-item>
-            <el-input v-model="vericode" placeholder="验证码" autocomplete="off"></el-input>
+            <el-input v-model="veriCode" placeholder="验证码" autocomplete="off"></el-input>
           </el-form-item>
         </el-form>
-        <div id="code">
+        <div v-bind:class="{ 'identify-code-div-style1': currentStep===1, 'identify-code-div-style2': currentStep!==1}">
           <identify :identifyCode = "identifyCode" :contentWidth = 180 :contentHeight = 48></identify>
           <span id="change-code-text" @click="refreshCode">换一张验证码</span>
         </div>
@@ -43,9 +44,14 @@
       return {
         identifyCodes: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
         identifyCode: '',
-        vericode: '',
+        veriCode: '',
         currentStep: 1,
-        dialogFormVisible: this.visible
+        contactInfo: '',
+        text_info: {
+          text1: '密码找回·第一步',
+          text2: '验证码将会发送至你的注册邮箱或手机'
+        },
+        input_box1_disabled: false
       };
     },
     methods: {
@@ -64,21 +70,47 @@
         }
       },
       nextStepClick: function() {
-        this.vericode = '';
+        // verify whether vericode the same as identycode
+        if (this.veriCode.toLowerCase() === this.identifyCode.toLowerCase()) {
+          // TODO: verify contact info
+          //var isContactInfoOK = (this.contactInfo === '944131226@qq.com');
+          var isContactInfoOK = true;
+          if (isContactInfoOK) {
+            this.text_info.text1 = '密码找回·第二步';
+            this.text_info.text2 = '验证码已发送，请在一分钟内完成验证';
+            this.currentStep = 2;
+            // document.getElementById('input-box1').disabled = true;
+            this.input_box1_disabled = true;
+          } else {
+            this.text_info.text2 = '手机号/邮箱不正确';
+          }
+        } else {
+          // console.log('wrong---', this.identifyCode);
+          this.text_info.text2 = '图片校验码错误，请重试';
+        }
+        this.veriCode = '';
       },
       hideDialog() {
         this.$emit('update:visible', false);
+        this.resetInfo();
+      },
+      resetInfo() {
+        this.currentStep = 1;
+        this.text_info.text1 = '密码找回·第一步';
+        this.text_info.text2 = '验证码将会发送至你的注册邮箱或手机';
+        this.input_box1_disabled = false;
+        this.contactInfo = '';
+        this.veriCode = '';
       }
     },
     mounted() {
-      this.currentStep = 0;
+      this.currentStep = 1;
       this.refreshCode();
     }
   };
 </script>
  
 <style scoped>
-
 .dialog-title {
   margin-left: 5%;
   margin-right: 5%;
@@ -100,9 +132,13 @@
   background-color: rgb(252, 251, 248);
 }
 
-
-#code {
+.identify-code-div-style1 {
   text-align: center;
+}
+
+.identify-code-div-style2 {
+  text-align: center;
+  display: none;
 }
 
 #change-code-text {
