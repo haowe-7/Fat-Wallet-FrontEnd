c347fa626122988e3a056d0bebed523c50666ebf
SYSUcarey <944131226@qq.com>
2019-06-27 01:05:55 +0800

    mock适配任务分类列表界面

--- src/components/CategoryPage.vue
+++ src/components/CategoryPage.vue
@@ -3,17 +3,21 @@
     <el-tabs v-model="activeName" type="border-card" @tab-click="handleClick">
       <el-tab-pane name="questionnaire">
         <span slot="label" class='category-sub-title'>问卷调查</span>
-        <div v-for="o in 4" :key="o" class="text item">
-          <TaskBlock></TaskBlock>
+        <div v-for="item in taskListWithType1" :key="item.task_id" class="text item">
+          <TaskBlock :task-info="item"></TaskBlock>
         </div>
       </el-tab-pane>
       <el-tab-pane name="association">
         <span slot="label" class='category-sub-title'>协会招新</span>
-        协会招新
+        <div v-for="item in taskListWithType2" :key="item.task_id" class="text item">
+          <TaskBlock :task-info="item"></TaskBlock>
+        </div>
       </el-tab-pane>
       <el-tab-pane name="delivery">
         <span slot="label" class='category-sub-title'>代取快递</span>
-        代取快递
+        <div v-for="item in taskListWithType3" :key="item.task_id" class="text item">
+          <TaskBlock :task-info="item"></TaskBlock>
+        </div>
       </el-tab-pane>
     </el-tabs>
   </div>
@@ -21,20 +25,73 @@
 
 <script>
 import TaskBlock from '@/components/TaskBlock';
+import { getTaskInfo } from '@/api/tasks'
 
 export default {
   name: 'CategoryPage',
   components: {
     TaskBlock
   },
+  beforeMount() {
+    let Loading = this.$loading({
+      lock: true,
+      text: '正快马加鞭帮小主查询',
+      spinner: 'el-icon-loading',
+      background: 'rgba(0, 0, 0, 0.7)'
+    });
+
+    getTaskInfo(this.queryJsons[0]).then(response => {
+      const status = response.status;
+      if (status === 200) {
+        console.log(this.queryJsons[0] + 'code:200');
+        console.log(response.data);
+        this.taskListWithType1 = response.data.data;
+      }
+      else
+        throw response.data.error;;
+    }).catch(err => {
+      this.$message.error(err);
+    })
+
+    getTaskInfo(this.queryJsons[1]).then(response => {
+      const status = response.status;
+      if (status === 200) {
+        console.log(this.queryJsons[1] + 'code:200');
+        console.log(response.data);
+        this.taskListWithType2 = response.data.data;
+      }
+      else
+        throw response.data.error;;
+    }).catch(err => {
+      this.$message.error(err);
+    })
+
+    getTaskInfo(this.queryJsons[2]).then(response => {
+      const status = response.status;
+      if (status === 200) {
+        console.log(this.queryJsons[2] + 'code:200');
+        console.log(response.data);
+        this.taskListWithType3 = response.data.data;
+        Loading.close();
+      }
+      else
+        throw response.data.error;;
+    }).catch(err => {
+      this.$message.error(err);
+    })
+  },
   data() {
     return {
+      queryJsons: ['{task_type:1}', '{task_type:2}', '{task_type:3}'],
       activeName: 'questionnaire',
+      taskListWithType1: [],
+      taskListWithType2: [],
+      taskListWithType3: [],
     };
   },
   methods: {
     handleClick(tab, event) {
-      console.log(tab, event);
+      // console.log(tab, event);
     }
   },
 };
